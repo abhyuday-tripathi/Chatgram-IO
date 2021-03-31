ccf62ec79522edcfaeeac0001dd6375f19b7a19c
Abhyuday Tripathi <tripathi.abhyuday2010@gmail.com>
2021-03-31 21:58:12 +0530

    Add alt to the images

--- src/App.js
+++ src/App.js
@@ -123,12 +123,7 @@ function ChatMessage(props) {
   return (
     <>
       <div className={`message ${messageClass}`}>
-        <img
-          src={
-            photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'
-          }
-          alt='Test'
-        />
+        <img src={photoURL} alt='ProfilePicture' />
         <p>{text}</p>
       </div>
     </>
